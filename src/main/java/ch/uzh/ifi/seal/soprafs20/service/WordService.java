e82ab2a6c9b5328047e4a7d8c1fbf0519576fe91
Ivan <ivan.allinckx@uzh.ch>
2020-04-14 14:15:24 +0200

    #FS20G13-59 Added Similarity Check

--- src/main/java/ch/uzh/ifi/seal/soprafs20/service/WordService.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/service/WordService.java
@@ -21,12 +21,15 @@ public class WordService {
             HttpResponse<String> response =
                     client.send(request, HttpResponse.BodyHandlers.ofString());
 
-            return new Gson().fromJson(response.body(), new TypeToken<ArrayList<LinkedTreeMap<String, String>>>(){}.getType());
-        } catch (Exception e) {
+            return new Gson().fromJson(response.body(), new TypeToken<ArrayList<LinkedTreeMap<String, String>>>() {
+            }.getType());
+        }
+        catch (Exception e) {
             System.out.println(e.getMessage());
             return new ArrayList<LinkedTreeMap<String, String>>();
         }
     }
+
     private static ArrayList<LinkedTreeMap<String, String>> trimArraylist(ArrayList<LinkedTreeMap<String, String>> oldList, int trimTo) {
         ArrayList<LinkedTreeMap<String, String>> newList = new ArrayList<LinkedTreeMap<String, String>>();
         if (oldList.size() > trimTo) {
@@ -38,9 +41,9 @@ public class WordService {
         return oldList;
     }
 
-    private static ArrayList<LinkedTreeMap<String, String>> removeMultipleWords(ArrayList<LinkedTreeMap<String, String>> oldList) {
+    private static ArrayList<LinkedTreeMap<String, String>> removeMultiWords(ArrayList<LinkedTreeMap<String, String>> oldList) {
         ArrayList<LinkedTreeMap<String, String>> newList = new ArrayList<LinkedTreeMap<String, String>>();
-        for (LinkedTreeMap<String, String> word :oldList) {
+        for (LinkedTreeMap<String, String> word : oldList) {
             if (!word.get("word").contains(" ")) {
                 newList.add(word);
             }
@@ -50,20 +53,33 @@ public class WordService {
 
 
     private static boolean isSimilar(String word1, String word2) {
-        if (word1.contains(word2) || word2.contains(word1)) {
+        return (word1.contains(word2) || word2.contains(word1) || isPlural(word1, word2) || isSameFamily(word1, word2));
+    }
+
+    private static boolean isSameFamily(String word1,String word2) {
+        return (word1.length() >= 5 && word2.length() >= 5 && word1.substring(0, 4).equals(word2.substring(0, 4)));
+    }
+    private static boolean isPlural(String word1, String word2) {
+        ArrayList<LinkedTreeMap<String, String>> word1request = getRequest("https://api.datamuse.com/words?md=d&max=1&sp=" + word1);
+        ArrayList<LinkedTreeMap<String, String>> word2request = getRequest("https://api.datamuse.com/words?md=d&max=1&sp=" + word2);
+        if (word1request.get(0).containsKey("defHeadword") && word1request.get(0).get("defHeadword").equals(word2)) {
             return true;
         }
-
+        else if (word2request.get(0).containsKey("defHeadword") && word2request.get(0).get("defHeadword").equals(word1)) {
+            return true;
+        }
+        else
+            return word1request.get(0).containsKey("defHeadword") && word2request.get(0).containsKey("defHeadword") &&
+                    word1request.get(0).get("defHeadword").equals(word2request.get(0).get("defHeadword"));
     }
 
     private static ArrayList<LinkedTreeMap<String, String>> getPossibleWords(ArrayList<LinkedTreeMap<String, String>> oldList) {
-        return trimArraylist(removeMultipleWords(oldList),5);
+        return trimArraylist(removeMultiWords(oldList), 5);
     }
 
     public static String getGoodWord(String word) {
         ArrayList<LinkedTreeMap<String, String>> wordList = new ArrayList<LinkedTreeMap<String, String>>();
         String domain = "http://api.datamuse.com/words?rel_spc=";
-        System.out.println(removeMultipleWords(getRequest(domain + word)));
 
         return "dora";
     }
